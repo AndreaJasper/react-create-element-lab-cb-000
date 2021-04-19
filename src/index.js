18a814cbcc334044c04c7b2140248deafef69575
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-04-19 16:00:31 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // IMPORTANT!!!! uncomment the lines below to make your tests work
-/*
+
+const meInReact =
+  React.createElement('div', { className: 'me' }, 
+    React.createElement('h1', {}, 'Andrea'),
+    React.createElement('p', )
+  );
+
 ReactDOM.render(
   meInReact,
   document.getElementById('global')
 );
 
 export default meInReact
-*/
+
